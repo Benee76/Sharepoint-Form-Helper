237f64dab6cc9b74cb65976e0517c41e95b3e054
Benee76 <80406237+Benee76@users.noreply.github.com>
2026-06-04 11:15:03 +0000

    d

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const repoName = "Sharepoint-Form-Helper";
+const base = process.env.NODE_ENV === "production" ? `/${repoName}/` : "/";
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base,
   plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
   },
-})
+});
